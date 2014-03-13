358fd50eb6d20dcf43440fe5413f4be564c31b3e
Andy---- <andypollard84@gmail.com>
2014-03-13 20:52:23 +0000

    Added Kiosk screens

---
+++ src/kioskScreens/KioskSearch.java
@@ -0,0 +1,117 @@
+package kioskScreens;
+
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+public class KioskSearch extends JFrame implements ActionListener
+{
+	private JFrame frame;
+	private String srcPath;
+	private JPanel main,center,top;
+	private JLabel logoLabel,empty1;
+	private JTextField search;
+	private JButton home,searchBtn;
+	private Font f1;
+	private ImageIcon hm,logo;
+	private GridBagConstraints gc;
+
+	public KioskSearch()
+	{
+		frame = new JFrame();
+		frame.setLayout(new BorderLayout());
+		frame.setSize(1000,600);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		frame.setUndecorated(true);
+		f1=new Font("Calibri",Font.PLAIN,20);
+		
+		srcPath = "src/kioskScreens/Resources/images/";
+		gc = new GridBagConstraints();
+		hm = new ImageIcon(srcPath+"home.png");
+		logo = new ImageIcon(srcPath+"logo.png");
+
+		main = new JPanel(new BorderLayout());
+		main.setBackground(Color.WHITE);
+		main.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+		frame.add(main);
+
+		top = new JPanel(new BorderLayout());
+		top.setBackground(new Color(0,0,0,0));
+		home = new JButton(hm);
+		home.setBackground(Color.WHITE);
+		home.setFont(new Font("Calibri",Font.BOLD,25));
+		home.setVerticalTextPosition(SwingConstants.BOTTOM );
+		home.setHorizontalTextPosition(SwingConstants.CENTER);
+		home.setBorder(null);
+		home.setPreferredSize(new Dimension(100,100));
+		home.addActionListener(this);
+		top.add(home, BorderLayout.WEST);
+		main.add(top,BorderLayout.NORTH);
+
+		center = new JPanel();
+		center.setLayout(new GridBagLayout());
+		center.setBackground(new Color(0,0,0,0));
+
+		//add logo to page center
+		logoLabel = new JLabel(logo);
+		logoLabel.setBorder(BorderFactory.createEmptyBorder(0,20,20,20));
+		gc.gridx = 0;
+		gc.gridy = 0;
+		center.add(logoLabel,gc);
+
+		//add search bar under logo
+		search = new JTextField(50);
+		search.setText("Search for a Product...");
+		search.setForeground(Color.LIGHT_GRAY);
+		search.setPreferredSize(new Dimension(1,35));
+		search.setFont(f1);
+		search.setBorder(BorderFactory.createLineBorder(new Color(102,178,255),1));
+		gc.gridx = 0;
+		gc.gridy = 1;
+		center.add(search,gc);
+
+		//add search button below search bar
+		empty1 = new JLabel(" ");
+		gc.gridx = 0;
+		gc.gridy = 2;
+		center.add(empty1,gc);
+		searchBtn = new JButton("Search");
+		searchBtn.setPreferredSize(new Dimension(160, 40));
+		searchBtn.setFont(new Font("Calibri",Font.BOLD,25));
+		searchBtn.setBackground(new Color(102,178,255));
+		searchBtn.setForeground(Color.white);
+		gc.gridx = 0;
+		gc.gridy = 3;
+		center.add(searchBtn,gc);
+		main.add(center,BorderLayout.CENTER);
+
+		search.addFocusListener(new FocusListener() {
+			public void focusGained(FocusEvent e) {
+				if(search.getText().trim().equals("Search for a Product..."))
+				{
+					search.setForeground(Color.BLACK);
+					search.setText("");
+				}
+			}
+			public void focusLost(FocusEvent e) {
+				search.setText("Search for a Product...");
+				search.setForeground(Color.LIGHT_GRAY);
+			}
+		});
+		frame.setVisible(true);
+		frame.setExtendedState(Frame.MAXIMIZED_BOTH);
+		searchBtn.requestFocus();
+	}
+
+	public void actionPerformed(ActionEvent e)
+	{
+		if(e.getSource()==home)
+		{
+			frame.dispose();
+		}
+	}
+
+}
---
+++ src/kioskScreens/KioskStartScreen.java
@@ -0,0 +1,123 @@
+package kioskScreens;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+
+import javax.swing.*;
+
+public class KioskStartScreen extends JFrame implements ActionListener
+{
+	private JFrame frame;
+	private String srcPath;
+	private JPanel content,header,footer;
+	private ImageIcon cn,hp,gm,mu,dvd,sd,src,dl,logo;
+	private JButton exit,con,headp,game,music,dvds,soundd,search,deals;
+	private JLabel logoLabel;
+	private JLabel empty1,empty2,empty3,empty4;
+
+	public KioskStartScreen()
+	{
+		frame = new JFrame();
+		frame.getContentPane().setBackground(Color.WHITE);
+		frame.setLayout(new BorderLayout());
+		frame.setSize(1000,600);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setUndecorated(true);
+		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		srcPath = "src/kioskScreens/Resources/images/";
+
+		cn = new ImageIcon(srcPath+"console.png");
+		hp = new ImageIcon(srcPath+"headphones.png");
+		gm = new ImageIcon(srcPath+"games.png");
+		mu = new ImageIcon(srcPath+"music.png");
+		dvd = new ImageIcon(srcPath+"dvd.png");
+		sd = new ImageIcon(srcPath+"soundDock.png");
+		src = new ImageIcon(srcPath+"search.png");
+		dl = new ImageIcon(srcPath+"deals.png");
+		logo = new ImageIcon(srcPath+"logo3.png");
+
+		empty1 = new JLabel(" ");
+		empty2 = new JLabel(" ");
+		empty3 = new JLabel(" ");
+		empty4 = new JLabel(" ");
+
+		con = new JButton(cn);
+		con.setBackground(Color.WHITE);
+		con.setBorder(null);
+		headp = new JButton(hp);
+		headp.setBackground(Color.WHITE);
+		headp.setBorder(null);
+		game = new JButton(gm);
+		game.setBackground(Color.WHITE);
+		game.setBorder(null);
+		music = new JButton(mu);
+		music.setBackground(Color.WHITE);
+		music.setBorder(null);
+		dvds = new JButton(dvd);
+		dvds.setBackground(Color.WHITE);
+		dvds.setBorder(null);
+		soundd = new JButton(sd);
+		soundd.setBackground(Color.WHITE);
+		soundd.setBorder(null);
+		search = new JButton(src);
+		search.setBackground(Color.WHITE);
+		search.setBorder(null);
+		search.addActionListener(this);
+		deals = new JButton(dl);
+		deals.setBackground(Color.WHITE);
+		deals.setBorder(null);
+
+		header = new JPanel(new BorderLayout());
+		exit = new JButton("Close");
+		exit.setBackground(new Color(238,238,238));
+		exit.setPreferredSize(new Dimension(100,50));
+		exit.addActionListener(this);
+		exit.setBorder(null);
+		header.add(exit,BorderLayout.EAST);
+		frame.add(header,BorderLayout.NORTH);
+
+		content = new JPanel(new GridLayout(2,6,0,5));
+		content.setBackground(new Color(0,0,0,0));
+		content.add(empty1);
+		content.add(search);
+		content.add(music);
+		content.add(dvds);
+		content.add(con);
+		content.add(empty2);
+		content.add(empty3);
+		content.add(game);
+		content.add(headp);
+		content.add(soundd);
+		content.add(deals);
+		content.add(empty4);
+		frame.add(content,BorderLayout.CENTER);
+
+		footer = new JPanel();
+		logoLabel = new JLabel(logo);
+		footer.add(logoLabel);
+		frame.add(footer,BorderLayout.SOUTH);
+
+		frame.setVisible(true);
+		frame.setExtendedState(Frame.MAXIMIZED_BOTH);
+	}
+	
+	public void actionPerformed(ActionEvent e)
+	{
+		if(e.getSource()==exit)
+		{
+			System.exit(0);
+		}
+		if(e.getSource()==search)
+		{
+			KioskSearch ks = new KioskSearch();
+		}
+	}
+	
+	public static void main(String args[])
+	{
+		KioskStartScreen kss = new KioskStartScreen();
+	}
+}
