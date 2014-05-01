c92f3a8a0918f54148e72e1fdd7f1612457e3bf7
John <johntk86@gmail.com>
2014-05-01 16:03:42 +0100

    Comments on EmployeeList

--- src/model/EmployeeList.java
+++ src/model/EmployeeList.java
@@ -15,9 +15,10 @@ public class EmployeeList {
 
 		this.ao = ao;
 		elist = new ArrayList<Employee>();
-
 	}
 
+	
+	//Removes employees from the ArrayList and re populates the ArrayList from the resultsSet
 	public void refreshList() {
 		rset = ao.getEmployee();
 
@@ -40,6 +41,8 @@ public class EmployeeList {
 		}
 	}
 
+	
+	//adds the last employee added to the DB to the Arraylist
 	public void addContact() {
 		rset = ao.getLastRow();
 		try {
@@ -57,15 +60,19 @@ public class EmployeeList {
 		}
 	}
 
+	//Returns an employee from the ArrayList based on the value i passed in
 	public Employee getEmployee(int i) {
 		refreshList();
 		return elist.get(i);
 	}
 
+	//returns the size of the ArrayList
 	public int getNumEmployee() {
 		return elist.size();
 	}
 
+	
+	//Deletes an employee from the ArrayList and from the DB
 	public int removeEmployee(int id) {
 		int num = 0;
 		
@@ -80,6 +87,7 @@ public class EmployeeList {
 		return num;
 	}
 
+	//Searches the ArrayList for and employee and returns the position in the array if found
 	public int findEmployee(String name) {
 		int index = -1;
 		for (int i = 0; i < elist.size(); i++) {
@@ -90,6 +98,7 @@ public class EmployeeList {
 		return index;
 	}
 
+	//Updates the Employee values associated with the passed in employee ID
 	public void updateEmployee(Employee e) {
 		for (int i = 0; i < elist.size(); i++) {
 			if (elist.get(i).getEmpID() == (e.getEmpID())) {
